51e80f0e8d9a2546ddb38d25baef7f99571ffa81
akobi <201678915+atkobi007@users.noreply.github.com>
2025-07-13 22:16:24 +0700

    fui-badge component

---
+++ uni_modules/fui-badge/components/fui-badge/fui-badge.vue
@@ -0,0 +1,147 @@
+<template>
+	<text v-if="text" :class="classNames" :style="[positionStyle, customStyle, dotStyle]"
+		class="fui-badge fui-badge--absolute" @click="onClick()">{{displayValue}}</text>
+</template>
+
+<script setup>
+	/**
+	 * Badge 数字角标  包裹的父节点必须设置 position
+	 * @description 数字角标一般和其它控件（列表、9宫格等）配合使用，用于进行数量提示，默认为实心灰色背景
+	 * @property {String} text 角标内容
+	 * @property {String} size = [normal|small] 角标内容
+	 * @property {String} type = [info|primary|success|warning|error] 颜色类型
+	 * 	@value info 灰色
+	 * 	@value primary 蓝色
+	 * 	@value success 绿色
+	 * 	@value warning 黄色
+	 * 	@value error 红色
+	 * @property {String} inverted = [true|false] 是否需背景颜色
+	 * @property {Number} maxNum 展示封顶的数字值，超过 99 显示 99+
+	 * @property {String} absolute = [rightTop|rightBottom|leftBottom|leftTop] 开启绝对定位, 角标将定位到其包裹的标签的四角上
+	 * 	@value rightTop 右上
+	 * 	@value rightBottom 右下
+	 * 	@value leftTop 左上
+	 * 	@value leftBottom 左下
+	 * @property {Array[number]} offset	距定位角中心点的偏移量，只有存在 absolute 属性时有效，例如：[-10, -10] 表示向外偏移 10px，[10, 10] 表示向 absolute 指定的内偏移 10px
+	 * @property {String} isDot = [true|false] 是否显示为一个小点
+	 * @event {Function} click 点击 Badge 触发事件
+	 * @example <fui-badge text="1"></fui-badge>
+	 */
+
+	const badgeRef = ref(null)
+	const emits = defineEmits("click");
+	const props = defineProps({
+		type: {
+			type: String,
+			default: 'error'
+		},
+		inverted: {
+			type: Boolean,
+			default: false
+		},
+		isDot: {
+			type: Boolean,
+			default: false
+		},
+		maxNum: {
+			type: Number,
+			default: 99
+		},
+		absolute: {
+			type: String,
+			default: 'rightTop'
+		},
+		offset: {
+			type: Array,
+			default () {
+				return [0, 0]
+			}
+		},
+		text: {
+			type: String,
+			default: ''
+		},
+		size: {
+			type: String,
+			default: 'small'
+		},
+		customStyle: {
+			type: Object,
+			default () {
+				return {}
+			}
+		}
+	})
+
+	const onClick = () => {
+		emits('click');
+	}
+
+	const classNames = computed(() => {
+		let type = 'fui-badge--' + props.type;
+		const inverted = props.inverted ? type + '-inverted' : type;
+		const absolute = props.absolute.length > 0 ? 'fui-badge--absolute' : '';
+		return [
+			inverted, absolute
+		].join(' ')
+	});
+
+	const positionStyle = computed(() => {
+		let xNum: Number = 0;
+		let yNum: Number = 0;
+		const offset = props.offset;
+		if (offset != null && offset.length > 0) {
+			xNum = offset[0] as Number;
+			yNum = offset[1] as Number;
+		}
+		const x = `${xNum}px`
+		const y = `${yNum}px`
+		const posList: any = {
+			rightTop: {
+				right: x,
+				top: y
+			},
+			rightBottom: {
+				right: x,
+				bottom: y
+			},
+			leftBottom: {
+				left: x,
+				bottom: y
+			},
+			leftTop: {
+				left: x,
+				top: y
+			}
+		}
+		const posStyle: any = posList[props.absolute];
+		console.error("TAG::",posStyle)
+		return posStyle
+	})
+
+	const dotStyle = computed(() => {
+		let dotResult: any = {}
+		if (!props.isDot) return dotResult
+		dotResult = {
+			width: '10px',
+			minWidth: '0',
+			height: '10px',
+			padding: '0',
+			borderRadius: '10px'
+		}
+		return dotResult
+	});
+
+	const displayValue = computed((): String => {
+		const textStr = props.text;
+		let textNumber = parseInt(textStr);
+		if (isNaN(textNumber) || props.isDot) {
+			return ""
+		}
+		return textNumber > props.maxNum ? `${props.maxNum}+` : textStr;
+	})
+</script>
+
+<style lang="scss">
+	@import "./index.scss"
+</style>
---
+++ uni_modules/fui-badge/readme.md
@@ -0,0 +1,1 @@
+# fui-badge

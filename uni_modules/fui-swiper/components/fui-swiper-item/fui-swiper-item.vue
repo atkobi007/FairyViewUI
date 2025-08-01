37001635e67f3330bbe14283d6e986e822cac3fc
akobi <201678915+atkobi007@users.noreply.github.com>
2025-08-01 21:27:10 +0700

    新增 fui-swiper 组件

---
+++ uni_modules/fui-swiper/components/fui-swiper-item/fui-swiper-item.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="fui-swiper-item" :style="bgStyle">
+		{{title}}-{{index}}--{{active}}
+	</view>
+</template>
+
+<script lang="uts" setup>
+	interface SwiperItemProps {
+		title : string
+		src : string
+		index : number
+		active : boolean
+	}
+
+	const props = withDefaults(defineProps<SwiperItemProps>(), {
+		title: "",
+		src: "",
+		index: 0,
+		active: true,
+	})
+
+	const bgStyle = computed(() => {
+		let obj : UTSJSONObject = new UTSJSONObject();
+		obj.set("background", `${props.active ? 'red' : 'gray'}`);
+		return obj;
+	})
+</script>
+
+<style lang="scss" scoped>
+	.fui-swiper-item {
+		width: 100%;
+		height: 100%;
+	}
+</style>
